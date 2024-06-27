3b5036ccabfee32d02228b42050c8629b88bcb63
Alexander Pride <a.pride@Alexanders-MacBook-Air.local>
2024-06-27 12:27:57 +0300

    feat: magic filters

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,4 +1,4 @@
-from aiogram import types, Router
+from aiogram import F, types, Router
 from aiogram.filters import CommandStart, Command
 
 user_private_router = Router()
@@ -9,21 +9,25 @@ async def start_cmd(message: types.message):
     await message.answer('Привеееет!')
 
 
+@user_private_router.message((F.text.lower().contains('продукты')) | (F.text.lower() == 'меню'))
 @user_private_router.message(Command('menu'))
 async def menu_cmd(message: types.message):
     await message.answer('Вот наше меню:')
 
 
+@user_private_router.message(F.text.lower() == 'о нас')
 @user_private_router.message(Command('about'))
 async def about_cmd(message: types.message):
     await message.answer('О нашей пиццерии:')
 
 
+@user_private_router.message((F.text.lower().contains('оплат')) | (F.text.lower() == 'способы оплаты'))
 @user_private_router.message(Command('payment'))
 async def payment_cmd(message: types.message):
     await message.answer('Способы оплаты:')
 
 
+@user_private_router.message((F.text.lower().contains('доставк')) | (F.text.lower() == 'варианты доставки'))
 @user_private_router.message(Command('shipping'))
 async def shipping_cmd(message: types.message):
     await message.answer('Варианты доставки:')
