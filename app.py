c3ac92c64e64d76179fabd68e40448451e3c9991
Alexander Pride <a.pride@Alexanders-MacBook-Air.local>
2024-06-27 10:56:40 +0300

    BotCommands

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import asyncio
 import os
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
 from handlers.user_private import user_private_router
+from common.bot_cmds_list import private
 
 ALLOWED_UPDATES = ['message, edit_message']
 
@@ -18,6 +19,7 @@ dp.include_router(user_private_router)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -12,3 +12,18 @@ async def start_cmd(message: types.message):
 @user_private_router.message(Command('menu'))
 async def menu_cmd(message: types.message):
     await message.answer('Вот наше меню:')
+
+
+@user_private_router.message(Command('about'))
+async def about_cmd(message: types.message):
+    await message.answer('О нашей пиццерии:')
+
+
+@user_private_router.message(Command('payment'))
+async def payment_cmd(message: types.message):
+    await message.answer('Способы оплаты:')
+
+
+@user_private_router.message(Command('shipping'))
+async def shipping_cmd(message: types.message):
+    await message.answer('Варианты доставки:')
